9bfc5063e68d23e092c9e6727033f70947b25630
Michał Mucek <michal.mucek@gmail.com>
2018-12-01 19:05:28 +0100

    Testowy ednpoint do zgłaszania szkody
    Dodaje:
    *ModuleController do obsługi endpoint'ów
    *testowy endpoint do zgłaszania szkody

---
+++ modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
@@ -0,0 +1,26 @@
+package edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych.Formularz;
+import edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych.Klient;
+
+@RestController
+public class ModuleController {
+
+    //Jedynie dla sprawdzenia, czy dział i działa :D
+    //Postman -> [POST] localhost:8080/api/damage_report?clientId=456&damageDescription=zalanie mieszkania
+    @RequestMapping(value = "/api/damage_report", method = RequestMethod.POST/*, produces = "application/json"*/)
+    public String postDamageReport(
+    @RequestParam(value = "clientId") int clientId, 
+    @RequestParam(value = "damageDescription") String damageDescription) {
+        
+        Klient klient = new Klient(0, clientId);
+        Formularz formularz = new Formularz(0, damageDescription, klient);
+
+        return formularz.getOpisSzkody();
+    }
+}
