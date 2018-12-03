959dbc4e691002def165dc40aa52c7b720248ea5
Michał Mucek <michal.mucek@gmail.com>
2018-12-03 14:26:33 +0100

    Usunięcie id z Klienta

--- modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/Klient.java
+++ modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/Klient.java
@@ -1,29 +1,17 @@
 package edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych;
 
 public class Klient {
-    int id;
     int nrKlienta;
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public void setNrKlienta(int nrKlienta) {
         this.nrKlienta = nrKlienta;
-    }
-
-    public int getId() {
-
-        return id;
     }
 
     public int getNrKlienta() {
         return nrKlienta;
     }
 
-    public Klient(int id, int nrKlienta) {
-
-        this.id = id;
+    public Klient(int nrKlienta) {
         this.nrKlienta = nrKlienta;
     }
 
