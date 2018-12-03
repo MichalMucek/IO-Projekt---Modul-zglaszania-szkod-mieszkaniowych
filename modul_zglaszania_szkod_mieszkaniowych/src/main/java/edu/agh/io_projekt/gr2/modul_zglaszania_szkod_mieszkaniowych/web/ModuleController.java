d7d82737943c3a98b00a1b8920110d334b5ec828
Michał Mucek <michal.mucek@gmail.com>
2018-12-03 13:57:10 +0100

    Wypchnięcie gałęzi

--- modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
+++ modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
@@ -11,6 +11,7 @@ import edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych.Klient;
 @RestController
 public class ModuleController {
 
+    //
     //Jedynie dla sprawdzenia, czy dział i działa :D
     //Postman -> [POST] localhost:8080/api/damage_report?clientId=456&damageDescription=zalanie mieszkania
     @RequestMapping(value = "/api/damage_report", method = RequestMethod.POST/*, produces = "application/json"*/)
