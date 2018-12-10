abab1a0541f93b4c5e8e5c2436bda22c1d59bcd4
Michał Mucek <michal.mucek@gmail.com>
2018-12-10 02:33:15 +0100

    Dodanie weryfikacji numeru klienta przy zgłaszaniu szkody

--- modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
+++ modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
@@ -11,16 +11,21 @@ import edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych.model.Klient
 @RestController
 public class ModuleController {
 
-    //Jedynie dla sprawdzenia, czy dział i działa :D
-    //Postman -> [POST] localhost:8080/api/damage_report?clientNumber=456&damageDescription=zalanie mieszkania
+    // Jedynie dla sprawdzenia, czy dział i działa :D
+    // Postman -> [POST]
+    // localhost:8080/api/damage_report?clientNumber=456&damageDescription=zalanie
+    // mieszkania
     @RequestMapping(value = "/api/damage_report", method = RequestMethod.POST)
     public String postDamageReport(
-    @RequestParam(value = "clientNumber") int clientNumber, 
-    @RequestParam(value = "damageDescription") String damageDescription) {
-        
-        Klient klient = new Klient(clientNumber);
-        Formularz formularz = new Formularz(0, damageDescription, klient);
+            @RequestParam(value = "clientNumber") int clientNumber,
+            @RequestParam(value = "damageDescription") String damageDescription) {
 
-        return formularz.getOpisSzkody();
+        Klient klient = new Klient(clientNumber);
+
+        if (klient.zweryfikujNumer()) {
+            Formularz formularz = new Formularz(1456, damageDescription, klient);
+            return formularz.getId() + " -> " + formularz.getOpisSzkody();
+        } else
+            return "Błędny numer klienta";
     }
 }
