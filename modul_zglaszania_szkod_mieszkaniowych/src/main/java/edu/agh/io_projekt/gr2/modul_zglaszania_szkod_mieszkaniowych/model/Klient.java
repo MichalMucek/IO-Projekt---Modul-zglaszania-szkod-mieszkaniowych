7718a5a5ac369acb1286c590311368cbb9d58eeb
Michał Mucek <michal.mucek@gmail.com>
2018-12-10 02:14:43 +0100

    Dodanie weryfikacji numeru klienta

--- modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/model/Klient.java
+++ modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/model/Klient.java
@@ -1,7 +1,10 @@
 package edu.agh.io_projekt.gr2.modul_zglaszania_szkod_mieszkaniowych.model;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Klient {
-    int nrKlienta;
+    Integer nrKlienta;
 
     public void setNrKlienta(int nrKlienta) {
         this.nrKlienta = nrKlienta;
@@ -21,5 +24,38 @@ public class Klient {
 
     void wypelnijFormularz(Formularz formularz) {
 
+    }
+
+    boolean zweryfikujNumer() {
+        boolean numerIstnieje = false;
+        
+        try {
+            URL baseUrl = new URL("https://nazwaubezpieczalni.pl/"); //TODO: Wpisać właściwy adres, gdy wszystko już będzie gotowe
+            URL url = new URL(baseUrl, nrKlienta.toString());
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+
+            Integer status = httpURLConnection.getResponseCode();
+            status = 200;
+            
+            switch (status) {
+                case 200: //OK
+                    numerIstnieje = true;
+                    break;
+                case 300: //Client with given ID does not exist
+                    //TODO: Exception() z kodem i wiadomością
+                    numerIstnieje = false;
+                    break;
+                default:
+                    //TODO: Obsługa kodów odpowiedzi 302, 303, 400, 500
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return numerIstnieje;
     }
 }
--- modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
+++ modul_zglaszania_szkod_mieszkaniowych/src/main/java/edu/agh/io_projekt/gr2/modul_zglaszania_szkod_mieszkaniowych/web/ModuleController.java
@@ -13,7 +13,7 @@ public class ModuleController {
 
     //Jedynie dla sprawdzenia, czy dział i działa :D
     //Postman -> [POST] localhost:8080/api/damage_report?clientNumber=456&damageDescription=zalanie mieszkania
-    @RequestMapping(value = "/api/damage_report", method = RequestMethod.POST/*, produces = "application/json"*/)
+    @RequestMapping(value = "/api/damage_report", method = RequestMethod.POST)
     public String postDamageReport(
     @RequestParam(value = "clientNumber") int clientNumber, 
     @RequestParam(value = "damageDescription") String damageDescription) {
